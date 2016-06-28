a1062508034b0df43268551511b3f2f6d2755deb
Ben <benlowry@outlook.com>
2016-06-28 16:17:55 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@
 # 3) dns server for friendly names on all the other stuff
 # 4) music player of some sort
 # 5) ssh key generation and outputting the public key after everything
-# 6) npm dependencies: jsbeautify, pm2
 # 7) heroku toolbelt
 
 # Setting up my chroot
@@ -80,7 +79,9 @@ sudo service deluged start
 
 # Install NodeJS
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.2/install.sh | bash
-~/.nvm/nvm.sh install v6.2.2
+source ~/.bashrc
+nvm install v6.2.2
+npm install -g pm2 jsbeautify
 
 # Install C9 IDE
 git clone git://github.com/c9/core ~/c9
