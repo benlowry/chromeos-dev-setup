af934a2e67ddf60bcefe3543801479672da47f60
Ben <benlowry@outlook.com>
2016-06-28 14:27:32 -0600

    Added todo list of missing features

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# TODO:
+# 1) automate startup of postgresql, c9, pgweb, deluge 
+# 2) install dropbox 
+# 3) dns server for friendly names on all the other stuff
+# 4) music player of some sort
+# 5) ssh key generation and outputting the public key after everything
+# 6) npm dependencies: jsbeautify, pm2
+# 7) heroku toolbelt
+
 # Setting up my chroot
 # Open a chrome terminal (ctrl+alt+t)
 # Download crouton to ~/Downloads
