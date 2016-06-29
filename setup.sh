574b1a2c27611c18a00ff2ca68fd721495dab478
Ben <benlowry@outlook.com>
2016-06-29 16:03:29 -0600

    More C9.io compatibility

--- setup.sh
+++ setup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 {
+  C9IO
+  GITWEBUI=false
+  POSTGRES=false
+  DELUGE=false
+  C9=false
+  PGWEB=false
+  DROPBOX=false
+  
+  if [ -z $C9_SHARED ]; then
+    C9IO=true
+  fi
+    
   # General dependencies
-  if [ -z `command -v add-apt-repository` ] || [ ! -z $C9_SHARED ]; then 
+  if [ -z `command -v add-apt-repository` ] || [ ! $C9IO = "true" ]; then 
     sudo apt-get update && sudo apt-get upgrade -y
     sudo apt-get install -y libssl-dev build-essential software-properties-common openssh-client man
     # TODO: per-package checks/installs  
@@ -26,6 +38,7 @@
     cd ~/
     git clone https://github.com/alberthier/git-webui.git
     git config --global alias.webui \!$PWD/git-webui/release/libexec/git-core/git-webui
+    GITWEBUI=true
   fi
 
   # Python
@@ -56,14 +69,17 @@
   fi
     
   # PostgreSQL, preinstalled on c9.io
-   if [[[ $@ == *"postgres"* ]] || [ -z $@ ]] && [ -z $C9_SHARED ]; then
-    sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
-    wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
-    sudo apt-get install -y postgresql postgresql-contrib
+   if [[ $@ == *"postgres"* ]] || [ -z $@ ]; then
+     if [ ! $C9IO = "true" ]; then
+        sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+        wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
+        sudo apt-get install -y postgresql postgresql-contrib
+    fi
+    POSTGRES=true
   fi
   
   # Install Deluge (torrent), disabled on c9.io
-   if [ -z $C9_SHARED ] && [[[ $@ == *"deluge"* ]] || [ -z $@ ]]; then
+   if [ ! $C9IO = "true" ] && [[[ $@ == *"deluge"* ]] || [ -z $@ ]]; then
     sudo add-apt-repository -y ppa:deluge-team/ppa
     sudo apt-get install -y deluge deluge-webui
     sudo adduser --disabled-password --system --home /var/lib/deluge --geeks "Deluge service" --group deluge
@@ -73,28 +89,32 @@
     sudo curl -o /etc/systemd/system/deluged.service https://raw.githubusercontent.com/benlowry/chromeos-setup/master/deluged.service
     sudo curl -o /etc/systemd/system/deluge-web.service https://raw.githubusercontent.com/benlowry/chromeos-setup/master/deluge-web.service
     sudo service deluged start
+    DELUGE=true
     # sudo service deluge-web start 
     # start: /usr/bin/deluge-web
   fi
   
   # Install C9 IDE, preinstalled on c9.io obviously 
-   if [ -z $C9_SHARED ] && [[[ $@ == *"c9"* ]] || [ -z $@ ]]; then
+   if [ ! $C9IO = "true" ] && [[[ $@ == *"c9"* ]] || [ -z $@ ]]; then
     git clone git://github.com/c9/core ~/c9
     cd ~/c9/scripts
     ./install-sdk.sh
     cd ~/
+    C9=true
     # start: node server.js -w ~/projectfolder --listen 0.0.0.0 --port=81
   fi
   
   # Install PGWeb
    if [[ $@ == *"pgweb"* ]] || [ -z $@ ]; then
     go get github.com/sosedoff/pgweb
+    PGWEB=true
     # start: $GOPATH/bin/pgweb —bind=0.0.0.0 —listen=82
   fi
 
   # Dropbox
    if [[ $@ == *"dropbox"* ]] || [ -z $@ ]; then
     cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+    DROPBOX=true
     # start: ~/.dropbox-dist/dropboxd
   fi
 
@@ -119,35 +139,40 @@
   echo $SSHKEY
   
   # startup notes and setup completion notes
-  if [[[ $@ == *"c9"* ]] || [ -z $@ ]] && [ -z $C9_SHARED ]; then
+  if [ $C9 = "true" ]; then
     echo '----------------------------------------'
     echo 'C9 browser IDE can be started with:'
     echo '$ node server.js -w ~/yourproject --listen 0.0.0.0 --port=81'
     echo 'Open in your browser at http://127.0.0.1:81/'
   fi
   
-  if [[ $@ == *"pgweb"* ]] || [ -z $@ ]; then
+  if [ $POSTGRES = "true" ]; then
+    echo 'PostgreSQL can be started with:'
+    echo '$ sudo service postgresql start'
+  fi
+  
+  if [ $PGWEB = "true" ]; then
     echo '----------------------------------------'
     echo 'PGWeb interface for PostgreSQL can be started with:'
     echo '$ $GOPATH/bin/pgweb —bind=0.0.0.0 —listen=82'
     echo 'Open in your browser at http://127.0.0.1:82/'
   fi
   
-  if [[ $@ == *"deluge"* ]] || [ -z $@ ]; then
+  if [ $DELUGE = "true" ]; then
     echo '----------------------------------------'
     echo 'Deluge torrent server and web interface can be started with:'
     echo '$ sudo user/bin/deluge-web --port 83'
     echo 'Open in your browser at http://127.0.0.1:83/'
   fi
    
-  if [[ $@ == *"git-webui"* ]] || [ -z $@ ]; then
+  if [ $GITWEBUI = "true" ]; then
     echo '----------------------------------------'
     echo 'Git WebUI can be started from your project directory with:'
     echo '$ git webui --port 84 --host 0.0.0.0 --no-browser'
     echo 'Open in your browser at http://127.0.0.1:84/'
   fi
   
-  if [[ $@ == *"dropbox"* ]] || [ -z $@ ]; then
+  if [ $DROPBOX = "true" ]; then
     echo '----------------------------------------'
     echo 'Dropbox setup can be completed by:'
     echo '$ ~/.dropbox-dist/dropboxd'
