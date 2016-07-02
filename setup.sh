980a8e23570e2aa1660a3828b17b56913f8d777b
Ben <benlowry@outlook.com>
2016-07-02 08:48:14 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -93,6 +93,7 @@
      if [ ! $C9IO = "true" ]; then
         sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
         wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
+        sudo apt-get update 
         sudo apt-get install -y postgresql postgresql-contrib
         sudo echo "service postgresql start" >> /etc/rc.local
     fi
