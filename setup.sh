777c858630653fd75fd983fc82aa1e92ccb53180
Ben <benlowry@outlook.com>
2016-07-02 14:56:35 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -82,7 +82,7 @@
         sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
         wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
         sudo apt-get update 
-        sudo apt-get install -y postgresql postgresql-contrib
+        sudo apt-get install -y postgresql postgresql-contrib libpq-dev
         sudo echo "service postgresql start" >> /etc/rc.local
     fi
     sudo service postgresql start
